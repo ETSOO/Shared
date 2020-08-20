18dfcae99557b6af95c1bb7a486a5899bd0f5408
garryxiao <garryxiao@gmail.com>
2020-08-20 19:31:53 +0800

    Fix version

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,6 @@
 export * from './DataTypes';
 export * from './DomUtils';
 export * from './ExtendUtils';
-export * from './HttpUtils';
 export * from './NumberUtils';
 export * from './StorageUtils';
 export * from './Utils';
