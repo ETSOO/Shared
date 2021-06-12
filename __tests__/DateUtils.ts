063fb3bab32914972184282d4621d30463359092
Garry Xiao <xz@etsoo.com>
2021-06-12 21:56:43 +1200

    Fix DateUtils tests UTC to local date

--- __tests__/DateUtils.ts
+++ __tests__/DateUtils.ts
@@ -2,10 +2,10 @@ import { DateUtils } from '../src/DateUtils';
 
 test('Tests for parse', () => {
     // Arrange
-    const date = new Date('2021-06-12T02:23:06Z');
+    const date = new Date('2021-06-12T02:23:06');
 
     // Act & assert
     expect(DateUtils.format('zh-CN', date, 'd')).toBe('2021/06/12');
-    expect(DateUtils.format('en-US', date, 'dm')).toBe('06/12/2021 14:23');
-    expect(DateUtils.format('en-NZ', date, 'ds')).toBe('12/06/2021 14:23:06');
+    expect(DateUtils.format('en-US', date, 'dm')).toBe('06/12/2021 02:23');
+    expect(DateUtils.format('en-NZ', date, 'ds')).toBe('12/06/2021 02:23:06');
 });
