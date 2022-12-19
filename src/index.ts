c4fdbc398e351f984a759de64ec5f49fa152adda
Garry Xiao <xz@etsoo.com>
2022-12-19 16:59:01 +1300

    Export ActionResult feature

--- src/index.ts
+++ src/index.ts
@@ -7,11 +7,13 @@ export * from './types/FormData';
 export * from './storage/IStorage';
 export * from './storage/WindowStorage';
 
+export * from './ActionResult';
 export * from './DataTypes';
 export * from './ColorUtils';
 export * from './DateUtils';
 export * from './DomUtils';
 export * from './ExtendUtils';
+export * from './IActionResult';
 export * from './Keyboard';
 export * from './NumberUtils';
 export * from './StorageUtils';
